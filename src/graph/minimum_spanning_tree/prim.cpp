639cdb6a1d6a69c68800337837990213b1d718f6
nicklaw <mzsrkeen10@gmail.com>
2017-06-25 15:23:37 +0900

    Reimplement prim.cpp using priority queue

--- src/graph/minimum_spanning_tree/prim.cpp
+++ src/graph/minimum_spanning_tree/prim.cpp
@@ -1,38 +1,44 @@
-#include <algorithm>
+/*
+    最小全域木(prim法)
+    時間計算量 O(|E|log|V|)
 
-#define MAX_E 10000
+    MAX_V : 頂点数の最大値
+
+    Usage:
+        cost[MAX_V][MAX_V]に各辺のコストを格納
+        Vに頂点数を格納
+        used[MAX_V]は初期化不要
+*/
+
+#include <algorithm>
+#include <queue>
+
 #define MAX_V 10000
 #define INF 1e9
 
+typedef pair<int, int> pii; // <cost, to>
+
 int cost[MAX_V][MAX_V]; // cost[u][v]は辺e=(u,v)のコスト(存在しない場合はINF)
-int mincost[MAX_V]; // 集合Xからの辺の最小コスト
-bool used[MAX_V];   // 頂点iがXに含まれているか
+bool used[MAX_V]; // 頂点iがXに含まれているか
 int V;
 
 int prim() {
   for (int i = 0; i < V; ++i) {
-    mincost[i] = INF;
     used[i] = false;
   }
-  mincost[0] = 0;
+  std::priority_queue<pii, vector<pii>, greater<pii> > que;
+  que.push(pii(0, 0));
   int res = 0;
 
-  while (true) {
-    int v = -1;
-    // Xに属さない頂点のうちXからの辺のコストが最小になる頂点を探す
-    for (int u = 0; u < V; u++) {
-      if (!used[u] && (v == -1 || mincost[u] < mincost[v]))
-        v = u;
-    }
-
-    if (v == -1)
-      break;
-    used[v] = true;    // 頂点vをXに追加
-    res += mincost[v]; // 辺のコストを加える
-
-    for (int u = 0; u < V; u++) {
-      mincost[u] = std::min(mincost[u], cost[v][u]);
-    }
+  while (!que.empty()) {
+    int cst = que.top().first, v = que.top().second;
+    que.pop();
+    if (used[v])
+      continue;
+    used[v] = true;
+    res += cst;
+    for (int u = 0; u < V; u++)
+      que.push(pii(cost[v][u], u));
   }
 
   return res;
