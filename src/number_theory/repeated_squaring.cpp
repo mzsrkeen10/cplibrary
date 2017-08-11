cbe8ab8f5233ce05975d8838daf97a1bdc68f524
Unknown <mzsrkeen10@gmail.com>
2017-08-11 19:07:08 +0900

    Add repeated squaring algorithm

---
+++ src/number_theory/repeated_squaring.cpp
@@ -0,0 +1,22 @@
+/*
+    累乗を求める(繰り返し二乗法)
+    時間計算量 O(log n)
+
+    Usage:
+        引数にx,nを与えるとx^n % MODを返す
+*/
+
+#define MOD 1000000007
+
+typedef long long ll;
+
+ll repeated_squaring(ll x, ll n) {
+  ll res = 1;
+  while (n > 0) {
+    if (n & 1)
+      res = res * x % MOD;
+    x = x * x % MOD;
+    n >>= 1;
+  }
+  return res;
+}
