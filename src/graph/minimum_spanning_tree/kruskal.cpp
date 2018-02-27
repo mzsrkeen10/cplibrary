554f91493bef9258ae45fdcf9af513b7a4d426c1
Unknown <mzsrkeen10@gmail.com>
2018-02-27 20:24:42 +0900

    Delete UFT functions from kruskal.cpp

--- src/graph/minimum_spanning_tree/kruskal.cpp
+++ src/graph/minimum_spanning_tree/kruskal.cpp
@@ -6,6 +6,7 @@
     MAX_N : Union-Find木の配列の要素数
 
     Usage:
+        ***UnionFindTreeクラスが必要***
         edge es[MAX_E]に辺を格納
         V,Eに頂点数,辺数を格納
 
@@ -24,45 +25,6 @@ struct edge {
     int u, v, cost;
 };
 
-int par[MAX_N];   // 親
-int depth[MAX_N]; // 木の深さ
-
-// n要素で初期化
-void init(int n) {
-    for (int i = 0; i < n; i++) {
-        par[i] = i;
-        depth[i] = 0;
-    }
-}
-
-// 木の根を求める
-int find(int x) {
-    if (par[x] == x) {
-        return x;
-    } else {
-        return par[x] = find(par[x]);
-    }
-}
-
-// xとyの属する集合を併合
-void unite(int x, int y) {
-    x = find(x);
-    y = find(y);
-    if (x == y)
-        return;
-
-    if (depth[x] < depth[y]) {
-        par[x] = y;
-    } else {
-        par[y] = x;
-        if (depth[x] == depth[y])
-            depth[x]++;
-    }
-}
-
-// xとyが同じ集合に属するか否か
-bool same(int x, int y) { return find(x) == find(y); }
-
 bool comp(const edge &e1, const edge &e2) { return e1.cost < e2.cost; }
 
 edge es[MAX_E];
@@ -70,12 +32,13 @@ int V, E; // 頂点数と辺数
 
 int kruskal() {
     std::sort(es, es + E, comp); // edge.costが小さい順にソートする
-    init(V);                     // Union-Findの初期化
+    UnionFindTree uft;
+    uft.init(V); // Union-Findの初期化
     int res = 0;
     for (int i = 0; i < E; i++) {
         edge e = es[i];
-        if (!same(e.u, e.v)) {
-            unite(e.u, e.v);
+        if (!uft.same(e.u, e.v)) {
+            uft.unite(e.u, e.v);
             res += e.cost;
         }
     }
