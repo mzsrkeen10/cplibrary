a886ed84b30d33a44077db414d4344a84142871f
Unknown <mzsrkeen10@gmail.com>
2018-07-05 17:10:15 +0900

    Verify extended euclid algorithm

--- src/number_theory/extended_euclidean.cpp
+++ src/number_theory/extended_euclidean.cpp
@@ -3,11 +3,18 @@
     時間計算量 O(log max(a,b))
 
     ax + by = 1を満たす整数x,yを求める
+
+    Usage:
+        解はx, yに入る
+
+    Verified:
+        AOJ NTL_1_E Extended Euclid Algorithm
 */
 
-long long int extgcd(long long int a, long long int b, long long int &x,
-                     long long int &y) {
-    long long int d = a;
+using i64 = long long int;
+
+i64 extgcd(i64 a, i64 b, i64 &x, i64 &y) {
+    i64 d = a;
     if (b != 0) {
         d = extgcd(b, a % b, y, x);
         y -= (a / b) * x;
