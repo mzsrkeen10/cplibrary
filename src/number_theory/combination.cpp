e7b6945538d4a7f6570a1cb408b237c3f2e715c1
Unknown <mzsrkeen10@gmail.com>
2018-07-05 17:45:02 +0900

    Replace long long int with i64

--- src/number_theory/combination.cpp
+++ src/number_theory/combination.cpp
@@ -3,37 +3,22 @@
 
     MAX_N : 配列の要素数の最大値
     MOD : 法
+
+    Usage:
+        *** extgcd, multi_invが必要 ***
 */
 
 #define MAX_N 10000
 #define MOD 1000000007
 
-long long int ncr[MAX_N];
+using i64 = long long int;
 
-long long int extgcd(long long int a, long long int b, long long int &x,
-                     long long int &y) {
-    long long int d = a;
-    if (b != 0) {
-        d = extgcd(b, a % b, y, x);
-        y -= (a / b) * x;
-    } else {
-        x = 1;
-        y = 0;
-    }
-    return d;
-}
+i64 ncr[MAX_N];
 
-// a : 元, md : 法
-long long int multi_inv(long long int md, long long int a) {
-    long long int x, y;
-    extgcd(md, a, x, y);
-    return (y > 0 ? y : md + y);
-}
-
-void set_ncr(long long int n) {
+void set_ncr(i64 n) {
     ncr[0] = 1;
-    for (long long int i = 1; i <= n; i++) {
-        long long int inv = multi_inv(i, MOD);
+    for (i64 i = 1; i <= n; i++) {
+        i64 inv = multi_inv(i, MOD);
         ncr[i] = (n - i + 1) * ncr[i - 1] % MOD;
         ncr[i] = ncr[i] * inv % MOD;
     }
--- src/number_theory/multiplicative_inverse.cpp
+++ src/number_theory/multiplicative_inverse.cpp
@@ -3,25 +3,15 @@
     時間計算量 O(log max(a,b))
 
     Usage:
+        *** extgcdが必要 ***
         gcd(a, md) == 1のときのみ乗法逆元が存在
 */
 
-long long int extgcd(long long int a, long long int b, long long int &x,
-                     long long int &y) {
-    long long int d = a;
-    if (b != 0) {
-        d = extgcd(b, a % b, y, x);
-        y -= (a / b) * x;
-    } else {
-        x = 1;
-        y = 0;
-    }
-    return d;
-}
+using i64 = long long int;
 
 // a : 元, md : 法
-long long int multi_inv(long long int md, long long int a) {
-    long long int x, y;
+i64 multi_inv(i64 md, i64 a) {
+    i64 x, y;
     extgcd(md, a, x, y);
     return (y > 0 ? y : md + y);
 }
