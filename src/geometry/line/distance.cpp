172ea040562f50599f642520ec65e532cb88115d
Unknown <mzsrkeen10@gmail.com>
2018-07-25 20:13:56 +0900

    Beautify distance.cpp

--- src/geometry/line/distance.cpp
+++ src/geometry/line/distance.cpp
@@ -10,12 +10,14 @@
         null
 */
 
-double distanceLP(Line line, Point p){
-    return abs(det(line.p2-line.p1, p-line.p1)) / abs(line.p2-line.p1);
+double distanceLP(Line line, Point p) {
+    return abs(det(line.p2 - line.p1, p - line.p1)) / abs(line.p2 - line.p1);
 }
 
-double distanceSP(Line line, Point p){
-    if(sgn(dot(line.p2-line.p1, p-line.p1)) < 0) return abs(p-line.p1);
-    if(sgn(dot(line.p1-line.p2, p-line.p2)) < 0) return abs(p-line.p2);
+double distanceSP(Line line, Point p) {
+    if (sgn(dot(line.p2 - line.p1, p - line.p1)) < 0)
+        return abs(p - line.p1);
+    if (sgn(dot(line.p1 - line.p2, p - line.p2)) < 0)
+        return abs(p - line.p2);
     return distanceLP(line, p);
 }
