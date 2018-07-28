1e09c32a107ae090ccf19dc7f881f2360f5b10d0
Unknown <mzsrkeen10@gmail.com>
2018-07-28 19:20:46 +0900

    Add distanceSS

--- src/geometry/line/distance.cpp
+++ src/geometry/line/distance.cpp
@@ -5,9 +5,10 @@
     Usage:
         点・線などを引数に与える
         template.cppが必要
+        distanceSSはccw.cpp, intersectLS.cppも必要
 
     Verified:
-        null
+        AOJ CGL_2_D Distance
 */
 
 double distanceLP(Line line, Point p) {
@@ -20,4 +21,14 @@ double distanceSP(Line line, Point p) {
     if (sgn(dot(line.p1 - line.p2, p - line.p2)) < 0)
         return abs(p - line.p2);
     return distanceLP(line, p);
+}
+
+double distanceSS(Line s1, Line s2) {
+    if (intersectLS(s1, s2, true))
+        return 0;
+    double d1 = distanceSP(s1, s2.p1);
+    double d2 = distanceSP(s1, s2.p2);
+    double d3 = distanceSP(s2, s1.p1);
+    double d4 = distanceSP(s2, s1.p2);
+    return min({d1, d2, d3, d4});
 }
