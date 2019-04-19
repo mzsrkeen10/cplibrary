4fd7474dd0d0d22b537906239ce3ca6d0c921a1f
Unknown <mzsrkeen10@gmail.com>
2019-04-19 17:01:45 +0900

    Add segment tree

---
+++ src/data_structure/tree/segment_tree.cpp
@@ -0,0 +1,73 @@
+/*
+    セグメント木
+    時間計算量 更新・取得ともにO(log(n))
+
+    モノイドに対する区間取得・一点更新
+
+    Usage:
+        size: セグメント木に乗せる列の大きさ
+        query_func: 取得用の関数
+        update_func: 更新用の関数
+        identity: 更新用の単位元
+        v: 列の初期値指定用
+
+    Verified:
+        AOJ DSL_2_A Range Minimum Query (RMQ)
+        AOJ DSL_2_B Range Sum Query
+*/
+
+template <typename T, typename E> class SegmentTree {
+  private:
+    int n;
+    vector<T> node;
+    const function<T(T, T)> query_func;
+    const function<T(T, E)> update_func;
+    const T identity;
+
+  public:
+    SegmentTree() = default;
+
+    SegmentTree(int size, function<T(T, T)> query_func,
+                function<T(T, E)> update_func, T identity,
+                vector<T> v = vector<T>())
+        : query_func(query_func), update_func(update_func), identity(identity) {
+        n = 1;
+        while (n < size)
+            n *= 2;
+        node.resize(2 * n - 1, identity);
+        if (size == v.size())
+            build(size, v);
+    }
+
+    void build(int size, vector<T> v) {
+        for (int i = 0; i < size; ++i)
+            node[i + n - 1] = v[i];
+        for (int i = n - 2; i >= 0; --i)
+            node[i] = query_func(node[2 * i + 1], node[2 * i + 2]);
+    }
+
+    void update(int x, E val) {
+        x += n - 1;
+
+        node[x] = update_func(node[x], val);
+        while (x > 0) {
+            x = (x - 1) / 2;
+            node[x] = query_func(node[2 * x + 1], node[2 * x + 2]);
+        }
+    }
+
+    T query(int a, int b, int k = 0, int l = 0, int r = -1) {
+        if (r < 0)
+            r = n;
+
+        if (r <= a || b <= l)
+            return identity;
+
+        if (a <= l && r <= b)
+            return node[k];
+
+        int vl = query(a, b, 2 * k + 1, l, (l + r) / 2);
+        int vr = query(a, b, 2 * k + 2, (l + r) / 2, r);
+        return query_func(vl, vr);
+    }
+};
