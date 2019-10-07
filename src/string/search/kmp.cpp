1fc33013559576a96ad3f8d5c6a11db17929fb3e
Unknown <mzsrkeen10@gmail.com>
2019-10-07 21:46:34 +0900

    Improve KMP algorithm

--- src/string/search/kmp.cpp
+++ src/string/search/kmp.cpp
@@ -3,7 +3,6 @@
     時間計算量 構築O(|P|),検索O(|T|)
 
     テキストTに含まれるパターンPの開始位置(0-indexed)を列挙する
-    正確にはMP法 ***そのうちKMP法にする***
 
     Usage:
         pattern: 検索するパターンP
@@ -26,31 +25,30 @@ class KMP {
     void build() {
         int m = pattern.size();
         table[0] = -1;
-        for (int j = -1, k = 0; k < m; ++k) {
-            while (j >= 0 && pattern[k] != pattern[j])
+        for (int i = 0, j = -1; i < m; ++i) {
+            while (j >= 0 && pattern[i] != pattern[j])
                 j = table[j];
-            table[k + 1] = ++j;
+            j++;
+            if (pattern[i + 1] == pattern[j])
+                table[i + 1] = table[j];
+            else
+                table[i + 1] = j;
         }
     }
 
     vector<int> search(const string &text) {
         int n = text.size(), m = pattern.size();
         vector<int> res;
-        for (int k = 0, i = 0; k + i < n;) {
-            if (text[k + i] == pattern[i]) {
-                i++;
-                if (i == m) {
-                    res.push_back(k);
-                    k += i - table[i];
-                    i = table[i];
-                }
-            } else {
-                k += i - table[i];
-                if (i > 0)
-                    i = table[i];
+        for (int i = 0, j = 0; j < n;) {
+            while (i > -1 && pattern[i] != text[j])
+                i = table[i];
+            i++;
+            j++;
+            if (i >= m) {
+                res.push_back(j - i);
+                i = table[i];
             }
         }
-
         return res;
     }
 };
