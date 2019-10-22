f0a4992afab77e1c83a145c9eb9c29dedd769242
Unknown <mzsrkeen10@gmail.com>
2019-10-22 17:31:54 +0900

    Add partition

---
+++ src/number_theory/counting/partition.cpp
@@ -0,0 +1,28 @@
+/*
+    分割数
+    時間計算量 O(nm)
+
+    nのm分割の個数を求める
+
+    Usage:
+        引数にn, mを与える
+        res[m][n] := nのm分割
+
+    Verified:
+        AtCoder dwacon2018-prelims-C Kill/Death
+*/
+
+vector<vector<i64>> set_part(int n, int m) {
+    vector<vector<i64>> res(m + 1, vector<i64>(n + 1, 0));
+    res[0][0] = 1;
+    for (int i = 1; i <= m; ++i) {
+        for (int j = 0; j <= n; ++j) {
+            if (j - i >= 0) {
+                res[i][j] = (res[i - 1][j] + res[i][j - i]) % MOD;
+            } else {
+                res[i][j] = res[i - 1][j];
+            }
+        }
+    }
+    return res;
+}
